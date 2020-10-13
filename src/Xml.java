d9f1cf854fbd85726fe77ac0e774435f11a29415
Bruna Gomes <brunaclegomes@hotmail.com>
2020-10-13 00:28:46 -0300

    Formatando saida

--- src/Xml.java
+++ src/Xml.java
@@ -1,12 +1,8 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.List;
 
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.basic.DateConverter;
 
 public class Xml extends LeitorDeArquivo{
 	
@@ -32,41 +28,58 @@ public class Xml extends LeitorDeArquivo{
 		xstream.alias("br.com.pageseguro.RemessaCartaoDebito", Remessa.class);
 		
 		this.remessasXml = (Remessa[]) xstream.fromXML(conteudoDoArquivo, lista);
+		
+		System.out.println("ARQUIVO LIDO: arquivos/remessa.xml");
+		System.out.println("---------------------------------------------------------\n");
 	}
 	
 	@Override
 	public void imprimeRemessa() {
 		
 		for(Remessa res: remessasXml) {
-			System.out.println("Nome: " + res.getNome());
-			System.out.println("CPF: " + res.getCPF());
-			System.out.println("Banco do Recebimento: " + res.getBancoRecebimento());
-			System.out.println("Banco do Pagamaento: " + res.getBancoPagamento());
-			System.out.println("Data da TransaÁ„o: " + res.getData());
-			System.out.printf( "%.2f", res.getValor() );
+			System.out.println("\n_____________________________________\n");
+			System.out.println("NOME: " + res.getNome());
 			
-			if (res.getNumeroBoleto() != null) {
-				// Se o valor do boleto  n„o È nulo
-				// Ent„o o cliente pagou com boleto
-				System.out.println("\nPAGO COM BOLETO:");
-				System.out.println("N˙m. Boleto: " + res.getNumeroBoleto());
-			}
-			else if (res.getParcelas() == 0) {
-				// Se o cliente n„o pagou com boleto ent„o pagou com cart„o
-				// Se n„o tem a quantidade de parcelas
-				// Ent„o o cliente pagou com cart„o de DÈbito
-				System.out.println("\nPAGO COM CART√O DE D…BITO:");
-				System.out.println("Nome TÌtular do Cart„o: " + res.getNomeTitular());
-				System.out.println("N˙m. Cart„o: " + res.getNumeroCartao());
-			}
+			String cpf = res.getCPF();
+			System.out.println("CPF: " + cpf);
+
+			VerificadorDeCPF verifCpf = new VerificadorDeCPF(cpf);
 			
+			if (verifCpf.verificaCPF() == true) {
+				System.out.println("---- CPF V√ÅLIDO ----\n");
+				
+				System.out.println("BANCO DO RECEBIMENTO: " + res.getBancoRecebimento());
+				System.out.println("BANCO DO PAGAMENTO: " + res.getBancoPagamento());
+				System.out.println("DATA DA TRANSA√á√ÉO: " + res.getData());
+				System.out.printf( "VALOR: %.2f", res.getValor() );
+				
+				if (res.getNumeroBoleto() != null) {
+					// Se o valor do boleto  n√£o √© nulo
+					// Ent√£o o cliente pagou com boleto
+					System.out.println("\n- PAGO COM BOLETO -");
+					System.out.println("N¬∫ BOLETO: " + res.getNumeroBoleto()+ '\n');
+				}
+				else if (res.getParcelas() == 0) {
+					// Se o cliente n√£o pagou com boleto ent√£o pagou com cart√£o
+					// Se n√£o tem a quantidade de parcelas
+					// Ent√£o o cliente pagou com cart√£o de D√©bito
+					System.out.println("\n- PAGO COM CART√ÉO DE D√âBITO -");
+					System.out.println("NOME TITULAR DO CART√ÉO: " + res.getNomeTitular());
+					System.out.println("N¬∫ CART√ÉO: " + res.getNumeroCartao() + '\n');
+				}
+				
+				else {
+					// Se n√£o o cliente pagou com o cart√£o de Cr√©dito
+					System.out.println("\n- PAGO COM CART√ÉO DE CR√âDITO -");
+					System.out.println("NOME TITULAR DO CART√ÉO: " + res.getNomeTitular());
+					System.out.println("N¬∫ CART√ÉO: " + res.getNumeroCartao());
+					System.out.println("QNT. PARCELAS: " + res.getParcelas() + '\n');
+				}
+			}
 			else {
-				// Se n„o o cliente pagou com o cart„o de CrÈdito
-				System.out.println("\nPAGO COM CART√O DE CR…DITO:");
-				System.out.println("Nome TÌtular do Cart„o: " + res.getNomeTitular());
-				System.out.println("N˙m. Cart„o: " + res.getNumeroCartao());
-				System.out.println("Qnt. Parcelas: " + res.getParcelas());
+				System.out.println("---- CPF INV√ÅLIDO ----\n");
 			}
+
 		}
 	}
 	
